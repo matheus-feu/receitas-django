008a6fabdff72525726cc0d00d42d427c682baa5
Havokzbr <matheusfeu@gmail.com>
2022-10-13 21:13:08 -0300

    fix

--- receitas/urls.py
+++ receitas/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', include('todas_receitas.urls')),
     path('usuarios/', include('usuarios.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT), \
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
