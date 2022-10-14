d3373da3f77d2a2fe791f762366b428d9d849872
Havokzbr <matheusfeu@gmail.com>
2022-10-13 21:30:20 -0300

    Carga Inicial

--- receitas/settings.py
+++ receitas/settings.py
@@ -118,10 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'receitas/static')
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
